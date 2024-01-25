9d6f2b1029415078844d2fe0c1b3a900cc925dc9
JasonJiaxiangLi <jasonljx96@gmail.com>
2024-01-25 23:08:25 +0000

    update bssh

--- test.sh
+++ test.sh
@@ -11,6 +11,15 @@
 #SBATCH --gres=gpu:a100:1
 #SBATCH -p a100-8
 
+# Benchmark info
+echo "TIMING - Starting jupyter at: $(date)"
+
 conda activate dec_mpi
+cd ~/jobsubmit/decen_mpi4py || exit
+echo "Job is starting on $(hostname)"
+which python3
+nvidia-smi
 
 mpirun -np 8 python run.py --data='cifar' --model='resnet' --updates=10001 --report=100 --comm_pattern='ring' --init_batch=1 --mini_batch=32 --step_type='constant' --k0=3 --beta=0.0228 --lr=10.0 --num_trial=1 --algorithm='dsgt'
+
+exit

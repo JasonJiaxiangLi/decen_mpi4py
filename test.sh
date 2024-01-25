6524bb8907f37d2076d4d2086fcacc01c1b553b7
JasonJiaxiangLi <jasonljx96@gmail.com>
2024-01-25 23:05:25 +0000

    minor updates

--- test.sh
+++ test.sh
@@ -13,4 +13,4 @@
 
 conda activate dec_mpi
 
-mpirun -np 8 python run.py --data='cifar' --model='resnet' --updates=10001 --report=100 --comm_pattern='random' --init_batch=1 --mini_batch=32 --step_type='constant' --k0=3 --beta=0.0228 --lr=10.0 --num_trial=1 --algorithm='dsgt'
+mpirun -np 8 python run.py --data='cifar' --model='resnet' --updates=10001 --report=100 --comm_pattern='ring' --init_batch=1 --mini_batch=32 --step_type='constant' --k0=3 --beta=0.0228 --lr=10.0 --num_trial=1 --algorithm='dsgt'

29e8597db808948f451415cce04acc5d67ca2dcf
JasonJiaxiangLi <jasonljx96@gmail.com>
2024-01-26 02:27:36 +0000

    update bash file

---
+++ test_mnist.sh
@@ -0,0 +1,9 @@
+mpirun -np 8 python run.py --data='mnist' --model='lenet' --updates=10001 --report=100 --comm_pattern='ring' --init_batch=1 --mini_batch=32 --step_type='constant' --k0=3 --beta=0.0228 --lr=500.0 --num_trial=1 --algorithm='dsgd'
+mpirun -np 8 python run.py --data='mnist' --model='lenet' --updates=10001 --report=100 --comm_pattern='ring' --init_batch=1 --mini_batch=32 --step_type='constant' --k0=3 --beta=0.0228 --lr=500.0 --num_trial=1 --algorithm='dgst'
+mpirun -np 8 python run.py --data='mnist' --model='lenet' --updates=10001 --report=100 --comm_pattern='ring' --init_batch=1 --mini_batch=32 --step_type='constant' --k0=3 --beta=0.0228 --lr=500.0 --num_trial=1 --algorithm='dasagt'
+mpirun -np 8 python run.py --data='mnist' --model='lenet' --updates=10001 --report=100 --comm_pattern='ring' --init_batch=1 --mini_batch=32 --step_type='constant' --k0=3 --beta=0.0228 --lr=500.0 --num_trial=1 --algorithm='dnasa'
+
+mpirun -np 8 python run.py --data='mnist' --model='lenet' --updates=10001 --report=100 --comm_pattern='ring' --init_batch=1 --mini_batch=32 --step_type='constant' --k0=3 --beta=0.0228 --lr=1000.0 --num_trial=1 --algorithm='dsgd'
+mpirun -np 8 python run.py --data='mnist' --model='lenet' --updates=10001 --report=100 --comm_pattern='ring' --init_batch=1 --mini_batch=32 --step_type='constant' --k0=3 --beta=0.0228 --lr=1000.0 --num_trial=1 --algorithm='dgst'
+mpirun -np 8 python run.py --data='mnist' --model='lenet' --updates=10001 --report=100 --comm_pattern='ring' --init_batch=1 --mini_batch=32 --step_type='constant' --k0=3 --beta=0.0228 --lr=1000.0 --num_trial=1 --algorithm='dasagt'
+mpirun -np 8 python run.py --data='mnist' --model='lenet' --updates=10001 --report=100 --comm_pattern='ring' --init_batch=1 --mini_batch=32 --step_type='constant' --k0=3 --beta=0.0228 --lr=1000.0 --num_trial=1 --algorithm='dnasa'

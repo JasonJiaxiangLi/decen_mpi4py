23c12a5f751c6bd3268c05f634e606e799f38af9
JasonJiaxiangLi <jasonljx96@gmail.com>
2024-01-30 20:50:12 +0000

    implement the ladder graph

--- create_mixing_matrix.py
+++ create_mixing_matrix.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def create_mix_mat(pattern, n, random_ratio=0.9):
+def create_mix_mat(pattern, n, random_ratio=0.8):
     if pattern == 'complete':
         res = np.ones((n, n)) / n
     elif pattern == 'ring':
@@ -18,8 +18,28 @@ def create_mix_mat(pattern, n, random_ratio=0.9):
                     res[i, idx] = 0.0
                     res[i, i] += 1 / n
     elif pattern == 'ladder':
-        # TODO: implement ladder
-        res = np.ones((n, n)) / n
+        if n % 2 != 0:
+            raise ValueError("For ladder graph, the number of nodes must be even")
+        res = np.eye((n, n))
+        for i in range(n // 2):
+            if i != 0:
+                res[i, i - 1] = 1 / n
+                res[i, i] -= 1 / n
+            if i != n // 2 - 1:
+                res[i, i + 1] = 1 / n
+                res[i, i] -= 1 / n
+            res[i, i + n // 2] = 1 / n
+            res[i, i] -= 1 / n
+
+            j = i + n // 2
+            if j != n // 2:
+                res[j, j - 1] = 1 / n
+                res[j, j] -= 1 / n
+            if j != n - 1:
+                res[j, j + 1] = 1 / n
+                res[j, j] -= 1 / n
+            res[j, j - n // 2] = 1 / n
+            res[j, j] -= 1 / n
     else:
         raise ValueError("This pattern is not defined")
 
